cb3b54ff6746f4a6d74eebea2ed464d124623225
18829342890 <18829342890@163.com>
2024-10-20 02:45:44 +0800

    init train_data project

---
+++ train_data_show/train_data/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_train_data_FILE D:/qt_project/train_data_show/train_data/build/Desktop_Qt_6_8_0_MinGW_64_bit-Debug/train_data.exe)
+set(__QT_DEPLOY_TARGET_train_data_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_train_data_RUNTIME_DLLS D:/QT58/6.8.0/mingw_64/bin/Qt6Charts.dll;D:/QT58/6.8.0/mingw_64/bin/Qt6OpenGLWidgets.dll;D:/QT58/6.8.0/mingw_64/bin/Qt6Widgets.dll;D:/QT58/6.8.0/mingw_64/bin/Qt6OpenGL.dll;D:/QT58/6.8.0/mingw_64/bin/Qt6Gui.dll;D:/QT58/6.8.0/mingw_64/bin/Qt6Core.dll)
